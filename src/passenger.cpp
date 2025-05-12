56189cd233d59934176d34031bb6ca0cb3c72970
SonaGurkina <sofagurkina080@gmail.com>
2025-05-12 21:49:53 +0300

    Update passenger.cpp

--- src/passenger.cpp
+++ src/passenger.cpp
@@ -1,2 +1,10 @@
 #include "../include/passenger.h"
 #include <iostream>
+
+Passenger::Passenger(const std::string& name, const std::string& passport, 
+                     const std::string& contact)
+    : name(name), passportNumber(passport), contactInfo(contact) {}
+
+std::string Passenger::getName() const { return name; }
+std::string Passenger::getPassportNumber() const { return passportNumber; }
+std::string Passenger::getContactInfo() const { return contactInfo; }
