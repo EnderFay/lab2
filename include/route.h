cd1bdf4acf1b3f5e1815661d1f385be7f9b2a195
SonaGurkina <sofagurkina080@gmail.com>
2025-05-12 22:26:54 +0300

    Update route.h

--- include/route.h
+++ include/route.h
@@ -3,7 +3,29 @@
 
 #include <string>
 #include <vector>
-#include "station.h"
 
+class Route {
+private:
+    int id;
+    std::string name;
+    int departureStationId;
+    int arrivalStationId;
+    std::vector<int> intermediateStations;
+    double distance;
+
+public:
+    Route(int id, const std::string& name, int depStationId, int arrStationId,
+          const std::vector<int>& intermediate, double distance);
+
+    int getId() const;
+    std::string getName() const;
+    int getDepartureStationId() const;
+    int getArrivalStationId() const;
+    std::vector<int> getIntermediateStations() const;
+    double getDistance() const;
+};
+
+    void setName(const std::string& newName);
+    void setDistance(double newDistance);
 
 #endif
