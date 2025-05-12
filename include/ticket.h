3f92e7e30f8abe5dc08b9670178e37a50fab7819
Anna-0502 <vinogradowaanna06@gmail.com>
2025-05-12 20:51:24 +0300

    Update ticket.h

--- include/ticket.h
+++ include/ticket.h
@@ -2,9 +2,28 @@
 #define TICKET_H
 
 #include <string>
-#include <vector>
-#include "passenger.h"
-#include "train.h"
 
+class Ticket {
+private:
+    int id;
+    int trainId;
+    std::tm bookingTime;
+    double price;
+    bool isReturned;
+    
+public:
+    Ticket(int id, int trainId, const Passenger& passenger, 
+           const std::tm& bookingTime, double price);
+    
+    int getId() const;
+    int getTrainId() const;
+    Passenger getPassenger() const;
+    
+    double getPrice() const;
+    
+   // void returnTicket();
+    
+    void displayInfo(const Train& train) const;
+};
 
-#endif
+#endif // TICKET_H
