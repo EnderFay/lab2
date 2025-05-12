b7bdd1d38cee93bb21e27bc6df4470cefab81cf9
SonaGurkina <sofagurkina080@gmail.com>
2025-05-12 21:44:06 +0300

    Update passenger.h

--- include/passenger.h
+++ include/passenger.h
@@ -2,6 +2,16 @@
 #define PASSENGER_H
 
 #include <string>
-#include <vector>
+
+class Passenger {
+private:
+    std::string name;
+    std::string passportNumber;
+    std::string contactInfo;
+
+public:
+    Passenger(const std::string& name, const std::string& passport, 
+              const std::string& contact);
+};
 
 #endif
