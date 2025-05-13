08c8e41818d1ab77bd778d7af0332b4260a50f1c
FaberGT2006 <romakorobov2464@gmail.com>
2025-05-13 22:05:13 +0300

    Update station.h

--- include/station.h
+++ include/station.h
@@ -1,8 +1,26 @@
-#ifndef STATION_H
-#define STATION_H
+#ifndef station_h
+#define station_h
 
 #include <string>
-#include <vector>
+#include <vector> 
 
+class Station {
+private:
+    int Id_; 
+    std::string nAme; 
+    std::string CiTy_;
+
+public:
+    Station(int a, const std::string& b, const std::string& c); 
+    
+    int getID() const; 
+    std::string GetName() const; 
+    std::string getcity() const; 
+
+    void setName(std::string newName); 
+    void SetCity(const std::string& City); 
+
+    void Display_Info() const; 
+};
 
 #endif
