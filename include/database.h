1da23c21a6677efd92a164885d0f72e5329907bf
Andrew <123974384+EnderFay@users.noreply.github.com>
2025-05-13 11:22:18 +0300

    Update database.h

--- include/database.h
+++ include/database.h
@@ -13,6 +13,8 @@ class Database {
 private:
     std::vector<Station> stations;
     std::vector<Route> routes;
+    std::vector<Train> trains;
+    std::vector<Ticket> tickets;
     
 public:
     void addStation(const Station& station);
@@ -28,6 +30,16 @@ public:
     const std::vector<Route>& getAllRoutes() const;
     Route* findRoute(int id);
     const Route* findRoute(int id) const;
+
+    void addTrain(const Train& train);
+    void removeTrain(int id);
+    void editTrain(int id, const std::string& number, int totalSeats);
+    const std::vector<Train>& getAllTrains() const;
+    Train* findTrain(int id);
+    const Train* findTrain(int id) const;
+    
+    void addTicket(const Ticket& ticket);
+    const std::vector<Ticket>& getAllTickets() const;
 };
 
 
