2baafc7b105a62af2e2795be32a432d81a11d37e
FaberGT2006 <romakorobov2464@gmail.com>
2025-05-14 18:00:17 +0300

    Update station.cpp

--- src/station.cpp
+++ src/station.cpp
@@ -1,23 +1,19 @@
 #include "../include/station.h"
 #include <iostream>
-int Station::getId() const { 
-    return id; 
-}
-std::string Station::getName() const { 
-    return name; 
-}
-std::string Station::getCity() const { 
-    return city; 
+
+Station::Station(int id, const std::string& name, const std::string& city)
+    : id(id), name(name), city(city) {
 }
 
-void Station::setName(const std::string& newName) 
-{ 
-    name = newName; 
-}
-void Station::setCity(const std::string& newCity){ city = newCity; }
-void Station::displayInfo() 
-{
-    std::cout << "Station ID: " << id <<"\n"
-              << "Name: "       << name <<"\n"
-              << "City: "       << city <<"\n"; 
+int Station::getId() const { return id; }
+std::string Station::getName() const { return name; }
+std::string Station::getCity() const { return city; }
+
+void Station::setName(const std::string& newName) { name = newName; }
+void Station::setCity(const std::string& newCity) { city = newCity; }
+
+void Station::displayInfo() const {
+    std::cout << "Station ID: " << id << "\n";
+    std::cout << "Name: " << name << "\n";
+    std::cout << "City: " << city << "\n";
 }
