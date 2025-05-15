7a69fc2ab8872539deba733898164e12d21e7179
FaberGT2006 <romakorobov2464@gmail.com>
2025-05-15 13:25:24 +0300

    Update train.h

--- include/train.h
+++ include/train.h
@@ -22,11 +22,11 @@ private:
     std::tm departureTime;
     std::tm arrivalTime;
     TrainStatus status;
-
+    
 public:
-    Train(int id, const std::string& number, int routeId, int totalSeats,
-        const std::tm& depTime, const std::tm& arrTime);
-
+    Train(int id, const std::string& number, int routeId, int totalSeats, 
+          const std::tm& depTime, const std::tm& arrTime);
+    
     int getId() const;
     std::string getNumber() const;
     int getRouteId() const;
@@ -36,18 +36,18 @@ public:
     std::tm getArrivalTime() const;
     TrainStatus getStatus() const;
     std::string getStatusString() const;
-
+    
     void setNumber(const std::string& newNumber);
     void setTotalSeats(int newTotalSeats);
     void setDepartureTime(const std::tm& newTime);
     void setArrivalTime(const std::tm& newTime);
     void setStatus(TrainStatus newStatus);
-
+    
     bool bookSeats(int count);
     bool cancelBooking(int count);
-
+    
     void displayInfo(const Route& route) const;
     void updateStatus(const std::tm& currentTime);
 };
 
-#endif 
+#endif
